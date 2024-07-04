45cd3030bef93e835958add95e5e1c48f70acc2e
fadel1608 <ardyatamafadel68@gmail.com>
2024-07-04 20:15:28 +0700

    tambah showmenu

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,6 @@
 import Nav from "../Components/Nav";
 import Hero from "../Components/Hero";
 import Service from "../Components/Service";
-import ShowMenu from "../Components/ShowMenu";
-import Customers from "../Components/Customers";
-
 
 
 export default function HomePage() {
