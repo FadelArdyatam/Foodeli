56531387f6aa2877752bed67848f5ecbff494ef5
fadel1608 <ardyatamafadel68@gmail.com>
2024-07-04 23:22:35 +0700

    update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,12 +3,14 @@ import Hero from "../Components/Hero";
 import Service from "../Components/Service";
 
 
+
 export default function HomePage() {
   return (
     <>
        <Nav/>
        <Hero/>
        <Service/>
+
     </>
   );
 }
