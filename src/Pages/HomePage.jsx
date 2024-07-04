9b1bde4fa3feb6fdff28c467c6c59602213a7f1a
wonkbrebes <denynz17@gmail.com>
2024-07-04 20:22:39 +0700

    Footer

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,13 +3,12 @@ import Hero from "../Components/Hero";
 import Service from "../Components/Service";
 
 
-
 export default function HomePage() {
   return (
     <>
-       <Nav/>
-       <Hero/>
-       <Service/>
+      <Nav />
+      <Hero />
+      <Service />
     </>
   );
 }
